56312ea0799ffa0e0dab0a2807bf6b9aeff82cc6
Praveen Kumar <praveensinghraghav96@gmail.com>
2017-03-09 20:17:20 +0530

    Icare App
    Full Application

---
+++ ICare/ICare/HomePage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ICare
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+            Uri vrij = new Uri("http://vrijraj.xyz/nearhost.html");
+            asd.Navigate(vrij);
+        }
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            // Only get results when it was a user typing, 
+            // otherwise assume the value got filled in by TextMemberPath 
+            // or the handler for SuggestionChosen.
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                //Set the ItemsSource to be your filtered dataset
+                //sender.ItemsSource = dataset;
+            }
+        }
+
+
+        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            // Set sender.Text. You can use args.SelectedItem to build your text string.
+        }
+
+
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (args.ChosenSuggestion != null)
+            {
+                // User selected an item from the suggestion list, take an action on it here.
+            }
+            else
+            {
+                // Use args.QueryText to determine what to do.
+            }
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        private void navigation_click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(WMR));
+        }
+
+        private void navigationToChild(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Birth));
+        }
+
+        private void navigationToHealth(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(HealthCare));
+        }
+
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ICare/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using System.Text;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ICare
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Uri sugga = new Uri("http://www.vrijraj.xyz/nearhost.html");
+            webview.Navigate(sugga);
+        }
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private async void first(object sender, RoutedEventArgs e)
+        {
+          //  User user = new User();
+          //  user.PersonId = Convert.ToInt16(username.Text);
+          //  user.LastName = password.Password;
+          //  HttpClient httpClient = new HttpClient();
+          //  httpClient.BaseAddress = new Uri("http://localhost:63298/");
+          //  httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            // Limit the max buffer size for the response so we don't get overwhelmed
+          //  httpClient.MaxResponseContentBufferSize = 266000;
+          //  string mydata = JsonConvert.SerializeObject(user);
+          //  var content = new StringContent(
+          //    mydata,
+           //   Encoding.UTF8,
+           //   "application/json");
+           // await httpClient.PostAsync("api/apis/addrecord", content);
+            this.Frame.Navigate(typeof(HomePage));
+        }
+
+        private void getreg(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Reg));
+        }
+
+        private void tip(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(HealthCare));
+        }
+        private void con(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(contact));
+        }
+    }
+}
